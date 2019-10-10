6e9e18694496b8ad97a53ac64250b65ce50d9efa
rahulgoyal911 <rahulgoyal0.rg@gmail.com>
2019-10-10 14:53:57 +0530

    join and leave room

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-
+from flask_socketio import join_room, leave_room
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
@@ -17,9 +17,22 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
 
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', room=room)
 if __name__ == '__main__':
     # socketio.run(app, debug=True,host = '127.0.0.1',port = 8089)
-    socketio.run(app, debug=True)
+    socketio.run(app,debug=True,host = '0.0.0.0',port = 80)
 
 
 # from flask_socketio import SocketIO, send
